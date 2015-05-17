4bd897f7e077432bdac8dd70542a12a9f10c81bf
Alex Fiannaca <fiannaca@cs.washington.edu>
2015-05-17 13:15:14 -0700

    fixed comment error

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
+//Note that in the callbacks the 'this' object is bound to our instance of the FlowViz object because
+// FlowViz extends EventEmitter!
 var App = new FlowViz.App('config.json', 'svg#InteractiveViz', {
 
-    //Note that the 'this' object is bound to our instance of the FlowViz object because FlowViz extends EventEmitter!
     ready: function() {
         this.Legend.Create('div#LeftSidebar');
     }
