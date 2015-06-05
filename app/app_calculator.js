42579e9a0487337b857d2d020f844832cd9ca6f4
Sonya Alexandrova <sonyaa@cs.washington.edu>
2015-06-05 11:54:58 -0700

    compute result for calculator on output click, not on validation

--- app/app_calculator.js
+++ app/app_calculator.js
@@ -9,33 +9,39 @@ var App = new FlowViz.App('config_calculator.json', 'svg#InteractiveViz', {
             this.Controls.Create('div#LeftSidebar');
             this.Legend.Create('div#LeftSidebar');
             this.DataEditor.Create('div#LeftSidebar');
-        }
-    },
+        },
 
-    GraphValidators: {
-        CalculateResult: function(nodes, edges) {
-            for (var j = 0; j < nodes.length; j++) {
-                nodes[j].input = [];
-            }
-            var ts = getTopSort(nodes);
-            for (var i = 0; i < ts.length; i++) {
-                if (ts[i].getOutgoingEdges().length == 0) {
-                    console.log('The result is: ' + ts[i].input[0]);
-                    break;
+        "selection-changed": function() {
+            var node = this.Selection.Current;
+            if (node.type.name === "Output") {
+                if (this.ConstraintChecker.IsValidGraph(this.GraphManager.nodes, this.GraphManager.edges)) {
+                    this.ShowMessage(calculateResult(this.GraphManager.nodes));
                 }
-                console.log(ts[i].type.type);
-                var value = 0;
-                if (ts[i].getIncomingEdges().length > 0) {
-                    value = compute(ts[i]);
-                } else {
-                    value = ts[i].GetDataItemValue('Value');
-                }
-                ts[i].getOutgoingEdges()[0].end.input.push(value);
             }
-            return true;
         }
     }
 });
+
+function calculateResult(nodes) {
+    for (var j = 0; j < nodes.length; j++) {
+        nodes[j].input = [];
+    }
+    var ts = getTopSort(nodes);
+    for (var i = 0; i < ts.length; i++) {
+        if (ts[i].getOutgoingEdges().length == 0) {
+            return ts[i].input[0];
+        }
+        console.log(ts[i].type.type);
+        var value = 0;
+        if (ts[i].getIncomingEdges().length > 0) {
+            value = compute(ts[i]);
+        } else {
+            value = ts[i].GetDataItemValue('Value');
+        }
+        ts[i].getOutgoingEdges()[0].end.input.push(value);
+    }
+    return null;
+}
 
 function compute(node) {
     if (node.type.name === "Addition") {
